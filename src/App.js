0d07ba1b4b013650ac4aa2f4de102d224e6b9073
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-11 11:12:06 -0500

    Routes work but no links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Procedures from './components/Procedures'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" exact component={Home}/>
+        <Route path="/Contact" component={Contact}/>           
+        <Route path="/Procedures" component={Procedures}/>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,6 @@
+function Contact() {
+    return (
+        <div>This is the Contact page</div>
+    )
+}
+export default Contact
